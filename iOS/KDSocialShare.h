fc5118c8d33191668bc5ec6b57e49e83357294ba
Deon Nicholas <dojiboy9@hotmail.com>
2017-01-30 20:30:02 -0800

    Fixes for react-native 0.40.0 and above

--- iOS/KDSocialShare.h
+++ iOS/KDSocialShare.h
@@ -7,8 +7,8 @@
 //
 @import UIKit;
 
-#import "RCTBridge.h"
-#import "RCTBridgeModule.h"
+#import <React/RCTBridge.h>
+#import <React/RCTBridgeModule.h>
 #import <MessageUI/MessageUI.h>
 
 @interface KDSocialShare : NSObject <RCTBridgeModule>
